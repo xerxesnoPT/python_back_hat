4cf3df0461bb7dcd44e7176ae7cb16c933d126ed
xerxeskali <xerxes@xerxes.site>
2020-07-27 04:42:41 -0400

    chatting room client

--- tcp/tcpclient.py
+++ tcp/tcpclient.py
@@ -4,34 +4,47 @@ import sys
 
 HOST = '172.16.164.1'
 PORT = 12333
+ADDR = (HOST,PORT)
 BUFSIZ = 1024
-ADDR = (HOST, PORT)
-tcpCliSock = socket(AF_INET, SOCK_STREAM)
-tcpCliSock.connect(ADDR)
-input_list = [tcpCliSock, sys.stdin]
+_current_in_list = [sys.stdin]
 
+def prompt():
+    sys.stdout.write('<you>')
+    sys.stdout.flush()
 
-#while 1:
-#    data = input('>>>')
-#    tcpCliSock.send(data.encode('utf-8'))
-#    data = tcpCliSock.recv(BUFSIZ)
-#    print(data.decode('utf-8'))
+def gen_message(channel_id, raw_message):
+    return '<RID:{}>{}</RID:{}>'.format(channel_id, raw_message, channel_id).encode('utf-8')
 
-while 1:
-    readyInput, readyOutut, readyException = select.select(input_list, [], [])
-    data = ''
-    for indata in readyInput:
-        if indata == tcpCliSock:
-            data = tcpCliSock.recv(BUFSIZ)
-            print(data.decode('utf-8'))
-            if data.decode('utf-8') == 'quit':
-                break
-        else:
-            data = input('>>>')
-            if data == 'quit':
-                tcpCliSock.send(data.encode('utf-8'))
-                break
-            tcpCliSock.send(data.encode('utf-8'))
-    if data=='quit':
-        break
-tcpCliSock.close()
+def main():
+    channel_id = input('<Channel ID>')
+    client_socket = socket()
+    try:
+        client_socket.connect(ADDR)
+        _current_in_list.append(client_socket)
+        client_socket.send(gen_message(channel_id,''))
+    except Exception as e:
+        print(e)
+        print('Connect failed')
+        sys.exit()
+
+    print('Connect to remote host')
+    prompt()
+
+    while 1:
+        readyInput, readyOutut, readyException = select.select(_current_in_list, [], [])
+        for indata in readyInput:
+            if indata is client_socket:
+                data = indata.recv(BUFSIZ)
+                if not data:
+                    print('disconnected from chat server')
+                    sys.exit()
+                else:
+                    sys.stdout.write(data.decode('utf-8')+'\r\n')
+                    prompt()
+            else:
+                raw_message = sys.stdin.readline()
+                client_socket.send(gen_message(channel_id, raw_message))
+                prompt()
+
+if __name__ == '__main__':
+    main()
