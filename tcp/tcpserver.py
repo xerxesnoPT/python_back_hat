94d482fce17c006e3949819c198d7a717795278b
xerxes <346287244@qq.com>
2020-07-27 13:44:57 +0800

    socker server use select

--- tcp/tcpserver.py
+++ tcp/tcpserver.py
@@ -1,5 +1,6 @@
 from socket import *
 from time import ctime
+import sys, select
 
 HOST = ''
 PORT = 12333
@@ -8,19 +9,32 @@ ADDR = (HOST, PORT)
 tcpSerSock = socket(AF_INET, SOCK_STREAM)
 tcpSerSock.bind(ADDR)
 tcpSerSock.listen(5)
+input_list = [tcpSerSock, sys.stdin]
 
 while 1:
     print('waitting for connectiong')
     tcpCliSock, addr = tcpSerSock.accept()
-    print('{} this is address'.format(addr)) 
-    data = ''
+    print('connect from addr {}'.format(addr))
+    input_list.append(tcpCliSock)
     while 1:
-        data = tcpCliSock.recv(BUFSIZ)
-        if not data or data.decode('utf-8') == 'quit':
+        readyinput, readyoutput, readyexception = select.select(input_list,[],[])
+        for indata in readyinput:
+            if indata == tcpCliSock:
+                data = tcpCliSock.recv(BUFSIZ)
+                if data.decode('utf-8') == 'quit':
+                    input_list.remove(tcpCliSock)
+                    break
+                print(data.decode('utf-8')) 
+            else:
+                data = input('>>')
+                if data == 'quit':
+                    tcpCliSock.send(data.encode('utf-8'))
+                    input_list.remove(tcpCliSock)
+                    break
+                tcpCliSock.send(data.encode('utf-8'))
+        if not isinstance(data, str):
+            data = data.decode('utf-8')
+        if data == 'quit':
             break
-        tcpCliSock.send('[{}]-{}'.format(ctime(),data).encode('utf-8'))
     tcpCliSock.close()
-    if data.decode('utf-8') == 'quit':
-        break
-
 tcpSerSock.close()
